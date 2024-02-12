2914d12f7c6fa3552c3c133f2776f32aa35c6795
mohammed sobhi <Mosobhi01@gmail.com>
2024-02-12 23:54:14 +0200

    first commit

---
+++ Targerian/script.js
@@ -0,0 +1,73 @@
+//step 1: get DOM
+let nextDom = document.getElementById("next");
+let prevDom = document.getElementById("prev");
+
+let carouselDom = document.querySelector(".carousel");
+let SliderDom = carouselDom.querySelector(".carousel .list");
+let thumbnailBorderDom = document.querySelector(".carousel .thumbnail");
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll(".item");
+let timeDom = document.querySelector(".carousel .time");
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+
+nextDom.onclick = function () {
+  showSlider("next");
+};
+
+prevDom.onclick = function () {
+  showSlider("prev");
+};
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+  next.click();
+}, timeAutoNext);
+function showSlider(type) {
+  let SliderItemsDom = SliderDom.querySelectorAll(".carousel .list .item");
+  let thumbnailItemsDom = document.querySelectorAll(
+    ".carousel .thumbnail .item"
+  );
+
+  if (type === "next") {
+    SliderDom.appendChild(SliderItemsDom[0]);
+    thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+    carouselDom.classList.add("next");
+  } else {
+    SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+    thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+    carouselDom.classList.add("prev");
+  }
+  clearTimeout(runTimeOut);
+  runTimeOut = setTimeout(() => {
+    carouselDom.classList.remove("next");
+    carouselDom.classList.remove("prev");
+  }, timeRunning);
+
+  clearTimeout(runNextAuto);
+  runNextAuto = setTimeout(() => {
+    next.click();
+  }, timeAutoNext);
+}
+/////////////
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the menu button and side-menu
+  const menuBtn = document.getElementById("menuBtn");
+  const sideMenu = document.querySelector(".side-menu");
+
+  // Add a click event listener to the menu button
+  menuBtn.addEventListener("click", function () {
+    // Toggle the display of the side-menu
+    sideMenu.style.right =
+      sideMenu.style.right === "0%" ? "-100%" : "0%";
+  });
+}); 
+//////////////////
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
